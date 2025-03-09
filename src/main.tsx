8c58e572bb27857ff62d10c3ccf9c6100934de33
Brian Ujiie <bujiie+github@gmail.com>
2025-03-08 16:46:39 -0800

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        <div>React App</div>
+    </StrictMode>
+);
