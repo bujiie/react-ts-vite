641fcf24c6d528f54c8f238abccefcb7e83672c3
Brian Ujiie <bujiie+github@gmail.com>
2025-03-09 17:48:32 -0700

    Add dummy App component

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import App from './App';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <div>React App</div>
+        <App />
     </StrictMode>
 );
